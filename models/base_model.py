91a18cfc7777c9da564b9d5f5842d67508145ecd
meddizzle316 <meddizzle316@gmail.com>
2024-02-01 09:26:20 -0600

    first go at 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 base class for all other classes
 in AirBnB project
 """
+from datetime import datetime
+import uuid
 
 
 class BaseModel:
@@ -11,3 +13,24 @@ class BaseModel:
     has common methods and attributes
     """
     
+    def __init__(self):
+        """init function for Basemodel"""
+        self.id = str(uuid.uuid4()) 
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """str representation of Basemodel"""
+        return f"[{self.__class__.__name__}] ({self.id} <{self.__dict__}>)"
+
+    def save(self):
+        """updates updated_at attribute"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """converts object to dict to prepare for json conversion"""
+        new_dict = vars(self) #or asdict method?
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
