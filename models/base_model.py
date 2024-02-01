72a791ebf6c0a7680137d26c3f696db281d37be6
meddizzle316 <meddizzle316@gmail.com>
2024-02-01 13:23:56 -0600

    add kwargs to init function

--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,19 @@ class BaseModel:
     has common methods and attributes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         init function for Basemodel adds 
         attributes
         """
-        self.my_number = 0
-        self.name = 'default'
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if not any(args) and len(kwargs) > 0:
+            kwargs.pop('__class__')
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        else:
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """
