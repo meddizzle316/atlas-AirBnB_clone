09c61736da0fe0446f59d7219b67a4930ab6da26
meddizzle316 <meddizzle316@gmail.com>
2024-02-04 08:21:28 -0600

    fixes to reload in task 5 -- almost done with updated_at and _created_at but not there

--- models/base_model.py
+++ models/base_model.py
@@ -21,15 +21,22 @@ class BaseModel:
         """
         if not any(args) and len(kwargs) > 0:
             kwargs.pop('__class__')
+            created_at_str = kwargs['created_at']
+            kwargs['created_at'] = datetime.fromisoformat(created_at_str)
+            updated_at_str = kwargs['updated_at']
+            kwargs['updated_at'] = datetime.fromisoformat(updated_at_str)
             for k, v in kwargs.items():
                 setattr(self, k, v)
+            print("kwargs has been used")
         else:
             self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.name = 'default'
             self.my_number = 2
-            storage.new() ## hopefully this is what  they wanted
+            storage.new(self) ## hopefully this is what  they wanted
+            print("kwargs has not been used")
+
     def __str__(self):
         """
         str representation of Basemodel
@@ -43,14 +50,13 @@ class BaseModel:
         uses datetime.now() to set time
         """
         storage.save()
-        self.updated_at = datetime.now()
+        # self.updated_at = datetime.now()
 
     def to_dict(self):
         """
         yet another attempt to 
         convert object to dictionary
         """
-        self.save()
         d = {}
         self.__dict__['__class__'] = self.__class__.__name__
         self.__dict__['updated_at'] = self.updated_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """a module for the class FileStorage"""
-from models.base_model import BaseModel
 import json
 import os
 
-class FileStorage(BaseModel):
+
+class FileStorage():
     """class that deals with file storage"""
     __file_path = 'file.json'
     __objects = {} #empty dictionary to hold everything by <class name>.id ex Basemodel.12121212
@@ -15,18 +15,31 @@ class FileStorage(BaseModel):
     
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        FileStorage.__objects[f"{type(obj).__name__}.{obj.id}"] = obj
 
     def save(self):
         """converts __objects to json string in __file_path"""
-        json.dump(FileStorage.__objects, FileStorage.__file_path)
+        dict_of_dict = {}
+        for key, value in self.__objects.items():
+            dict_of_dict[key] = value.to_dict() 
+        with open(self.__file_path, "w") as file:
+            json.dump(dict_of_dict, file, indent=4)
 
+
+    def classes(self):
+        """returns a dictionary of valid classes"""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+        return classes                
+        
     def reload(self):
         """converts json strings in __file_path to __objects"""
         if not os.path.isfile(f"{FileStorage.__file_path}"):
             pass
         else:
             with open(f"{FileStorage.__file_path}", 'r') as file:
-                python_dict = json.loads(file.read()) #I think the return python object is a dict, not sure
-                for key, value in python_dict.items():
-                    setattr(FileStorage.__objects, key, value)
+                python_dict = json.loads(file.read()) 
+                for id, dictionary in python_dict.items():
+                    class_name = dictionary['__class__']
+                    FileStorage.__objects[id] = self.classes()[class_name](**dictionary)
