a18bfbba1072e423d28f63b29340f286ca78d054
Greentaylor27 <7196@holbertonstudents.com>
2024-02-04 12:44:01 -0600

    Removing formatting issues to hopeful fix some issues

--- models/user.py
+++ models/user.py
@@ -6,14 +6,11 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    email, password, first_name, last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get("email", "")
-        self.password = kwargs.get("password", "")
-        self.first_name = kwargs.get("first_name", "")
-        self.last_name = kwargs.get("last_name", "")
+    def __init__(self, email, password, first_name, last_name):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
 
     def to_dict(self):
         return {
