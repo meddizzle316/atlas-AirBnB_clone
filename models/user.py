5ccf1d9fa7b6010ae89309f9a1137dacf07a30a9
Greentaylor27 <7196@holbertonstudents.com>
2024-02-04 14:43:46 -0600

    Adding conditionals to see if this changes the checker

--- models/user.py
+++ models/user.py
@@ -7,13 +7,33 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     # email, password, first_name, last_name = ''
+    # email = ''
+    # password = ''
+    # first_name = ''
+    # last_name = ''
+    
+    def __init__(self, **kwargs):
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args)
-        self.email = kwargs.get('email', '')
-        self.password = kwargs.get('password', '')
-        self.first_name = kwargs.get('first_name', '')
-        self.last_name = kwargs.get('last_name', "")
+        if not isinstance(email, str):
+            raise TypeError("Email must be a string")
+        else:
+            self.email = kwargs.get('email', '')
+        if not isinstance(password, str):
+            raise TypeError("Password must be a string")
+        else:
+            self.password = kwargs.get('password', '')
+        if not isinstance(first_name, str):
+            raise TypeError("invalid First name")
+        else:
+            self.first_name = kwargs.get('first_name', '')
+        if not isinstance(last_name, str):
+            raise TypeError("invalid last name")
+        else:
+            self.last_name = kwargs.get('last_name', "")
 
         def to_dict(self):
             return {
