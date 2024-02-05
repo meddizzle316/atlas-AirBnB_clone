0ccd5d9d8feb35188d5810dda3d54b93b2b3b84b
Greentaylor27 <7196@holbertonstudents.com>
2024-02-05 11:57:13 -0600

    Updating state

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ Class that holds the information of State for Airbnb console"""
     name = ''
+    id = ''
 
     def to_dict(self):
         return {
