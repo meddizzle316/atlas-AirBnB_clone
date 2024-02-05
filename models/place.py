08a2c79280e0cc813ceca55ed03f415de7389f75
Greentaylor27 <7196@holbertonstudents.com>
2024-02-05 11:56:28 -0600

    First commit of Place

---
+++ models/place.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that stores location data"""
+from models.base_model import BaseModel
+
+
+
+class Place(BaseModel):
+    """
+    Class that stores location information
+    """
