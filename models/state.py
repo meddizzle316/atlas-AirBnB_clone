defed3ea0e530f7facf0855839b306bcdce31e56
Greentaylor27 <7196@holbertonstudents.com>
2024-02-05 11:14:41 -0600

    Class for state

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+""" Module that for the class of State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class that holds the information of State for Airbnb console"""
+    name = ''
+
+    def to_dict(self):
+        return {
+            "id": id,
+            "name": State.name
+        }
