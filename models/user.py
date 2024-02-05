b8e631a51ba301c2a5f2c02ea3ba3d50e833f7b6
Greentaylor27 <7196@holbertonstudents.com>
2024-02-05 10:52:11 -0600

    After speaking with partner we are going back to basics

--- models/user.py
+++ models/user.py
@@ -2,14 +2,28 @@
 """
 A class to store user data
 """
-import email
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    class that stores User data
+    """
     
     def __init__(self):
+        """
+        Initialization
+        """
         email = ''
         password = ''
         first_name = ''
         last_name = ''
+
+        def to_dict(self):
+            return {
+                "id": id,
+                "email": email,
+                "password": password,
+                "first_name": first_name,
+                "last_name": last_name
+            }
