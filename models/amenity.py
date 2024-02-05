dcdb0ffe73801c1b04e2373af8747a0875003abf
Greentaylor27 <7196@holbertonstudents.com>
2024-02-05 11:56:01 -0600

    First push of amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that stores amenity information"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class that Stores amenity data """
+    
