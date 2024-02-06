f09291815108ebf3a25fc89b62079bf86cfa3544
meddizzle316 <meddizzle316@gmail.com>
2024-02-06 11:31:53 -0600

    cleaned up classe and fix all command?

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module that stores amenity information"""
 from models.base_model import BaseModel
-from datetime import datetime
 
 class Amenity(BaseModel):
     """ Class that Stores amenity data """
@@ -11,15 +10,5 @@ class Amenity(BaseModel):
         """
         Initialization of the State class
         """
-        if not any(args) and len(kwargs) > 0:
-            kwargs.pop('__class__')
-            created_at_str = kwargs['created_at']
-            kwargs['created_at'] = datetime.fromisoformat(created_at_str)
-            updated_at_str = kwargs['updated_at']
-            kwargs['updated_at'] = datetime.fromisoformat(updated_at_str)
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        else:
-            super().__init__()
-            self.name = ''
+        super().__init__(*args, **kwargs)
     
--- models/review.py
+++ models/review.py
@@ -15,16 +15,4 @@ class Review(BaseModel):
         """
         Initialization of the State class
         """
-        if not any(args) and len(kwargs) > 0:
-            kwargs.pop('__class__')
-            created_at_str = kwargs['created_at']
-            kwargs['created_at'] = datetime.fromisoformat(created_at_str)
-            updated_at_str = kwargs['updated_at']
-            kwargs['updated_at'] = datetime.fromisoformat(updated_at_str)
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        else:
-            super().__init__()
-            self.place_id = '' #will be Place.id
-            self.user_id = '' #will be User.id
-            self.text = ''
+        super().__init__(*args, **kwargs)
